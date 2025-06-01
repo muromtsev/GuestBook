d05cf3df8d4bbf5a41c94e45b4a71f4a142ac6ba
muromtsev <t4k.org@mail.ru>
2025-06-01 23:45:28 +0300

    added styles, avatar

--- src/main/java/com/example/guestbook/Entity/GuestBookEntry.java
+++ src/main/java/com/example/guestbook/Entity/GuestBookEntry.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.util.DigestUtils;
 
 import java.time.LocalDateTime;
 
@@ -20,8 +21,18 @@ public class GuestBookEntry {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String authorName;
+    private String email;
     private String message;
     private LocalDateTime createdAt;
 
+
+    public String getAvatarUrl() {
+        if (email != null && !email.isEmpty()) {
+            String hash = DigestUtils.md5DigestAsHex(email.toLowerCase().getBytes());
+            return "https://www.gravatar.com/avatar/" + hash + "?d=identicon";
+        } else {
+            return "https://api.dicebear.com/7.x/identicon/svg?seed=" + authorName;
+        }
+    }
 
 }
