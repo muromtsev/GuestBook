ac6721c478709ff423e1e11ffc37b2ff9a213c4a
muromtsev <t4k.org@mail.ru>
2025-06-02 00:14:47 +0300

    added validation

--- src/main/java/com/example/guestbook/Controller/GuestBookController.java
+++ src/main/java/com/example/guestbook/Controller/GuestBookController.java
@@ -2,9 +2,11 @@ package com.example.guestbook.Controller;
 
 import com.example.guestbook.Entity.GuestBookEntry;
 import com.example.guestbook.Service.GuestBookService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,12 +25,21 @@ public class GuestBookController {
     @GetMapping
     public String index(Model model) {
         List<GuestBookEntry> entries = guestBookService.getGuestBookEntries();
+        model.addAttribute("entry", new GuestBookEntry());
         model.addAttribute("entries", entries);
         return "index";
     }
 
     @PostMapping
-    public String addEntry(@ModelAttribute("entry") GuestBookEntry entry) {
+    public String addEntry(@Valid @ModelAttribute("entry") GuestBookEntry entry,
+                           BindingResult bindingResult,
+                           Model model)
+    {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("errors", bindingResult.getAllErrors());
+            return "index";
+        }
+
         entry.setCreatedAt(LocalDateTime.now());
         guestBookService.saveGuestBookEntry(entry);
         return "redirect:/";
--- src/main/java/com/example/guestbook/Entity/GuestBookEntry.java
+++ src/main/java/com/example/guestbook/Entity/GuestBookEntry.java
@@ -1,9 +1,9 @@
 package com.example.guestbook.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -20,8 +20,13 @@ public class GuestBookEntry {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Имя не может быть пустым")
+    @Size(min = 2, max = 50, message = "Имя должно быть от 2 до 50 символов")
     private String authorName;
+    @Email(message = "Некорректный email")
     private String email;
+    @NotBlank(message = "Сообщение не может быть пустым")
+    @Size(min = 10, max = 1000, message = "Сообщение должно быть от 10 до 1000 символов")
     private String message;
     private LocalDateTime createdAt;
 
